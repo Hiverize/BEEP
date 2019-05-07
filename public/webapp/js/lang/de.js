955d8e9f9dd0b1b0faa7e70b004c2702525674ab
direnS <diren@diren.de>
2019-05-07 20:49:19 +0200

    de lang bugfix

--- public/webapp/js/lang/de.js
+++ public/webapp/js/lang/de.js
@@ -20,9 +20,7 @@ LANG['de'] =
 	Website : 'Webseite',
 	Feedback : 'Feedback',
 	Feedback_mail_header : 'Bob app feedback',
-	Feedback_mail_body : '
-Liebes Bob Team,%0D%0A%0D%0Ahier ist mein Feedback zur Bob-App.%0D%0A%0D%0AIch habe das beobachtet: this:%0D%0A%0D%0ABevor das passiert ist, habe ich das gemacht:%0D%0A%0D%0A Der Bildschirm hat so ausgesehen (bitte Screenshot hinzufügen):%0D%0A%0D%0A%0D%0A%0D%0A
-Dear Bob Team,%0D%0A%0D%0AHereby my feedback about the Bob app.%0D%0A%0D%0AI discovered this:%0D%0A%0D%0AJust before that happened, i did:%0D%0A%0D%0AThe screen was looking like (please include a screenshot):%0D%0A%0D%0A%0D%0A%0D%0A',
+	Feedback_mail_body : 'Liebes Bob Team,%0D%0A%0D%0Ahier ist mein Feedback zur Bob-App.%0D%0A%0D%0AIch habe das beobachtet: this:%0D%0A%0D%0ABevor das passiert ist, habe ich das gemacht:%0D%0A%0D%0A Der Bildschirm hat so ausgesehen (bitte Screenshot hinzufügen):%0D%0A%0D%0A%0D%0A%0D%0A Dear Bob Team,%0D%0A%0D%0AHereby my feedback about the Bob app.%0D%0A%0D%0AI discovered this:%0D%0A%0D%0AJust before that happened, i did:%0D%0A%0D%0AThe screen was looking like (please include a screenshot):%0D%0A%0D%0A%0D%0A%0D%0A',
 	Diagnostic_info : 'Diagnostic information (in case of a bug):%0D%0A',
 	back : 'Zurück',
 	menu : 'Menü',
